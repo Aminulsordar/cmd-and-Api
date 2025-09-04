dd29b4b1144295f8b301b6444b18102da4054569
Aminul-sordar <132671015+Aminulsordar@users.noreply.github.com>
2025-09-05 00:20:37 +0600

    Update goat-poli.js

--- goat-poli.js
+++ goat-poli.js
@@ -5,7 +5,7 @@ const path = require("path");
 module.exports = {
   config: {
     name: "poli",
-    aliases: ["pinterest"],
+    aliases: ["poli2"],
     version: "1.0.0",
     author: "Aminulsordar",
     countDown: 5,
